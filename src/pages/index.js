26e4171f9c41018baedd1e2049768309b8a93f0f
Shubham Tiwari <shubhmtiwri00@gmail.com>
2023-03-10 14:24:56 +0530

    Redirected the main page to feedback page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
+import { useRouter } from "next/router"
 import Head from 'next/head'
 
-
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push("/feedback")
+  },[])
   return (
     <>
       <Head>
